e1abd9d527803354dabca8b7877fd368053b75a1
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-08-10 12:02:04 -0700

    minor updates to paths

--- server/index.js
+++ server/index.js
@@ -2,23 +2,27 @@ const express =  require('express');
 const path = require('path');
 const bodyparser = require('body-parser');
 
+const port = 3000;
+
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.use('/', express.static(path.join(__dirname, './../client/dist')));
 
 
-app.get('/',(req, res) =>{
-
-});
-
-app.post('/',(req, res) =>{
+app.get('/api',(req, res) =>{
 
 });
 
-app.put('/',(req, res) =>{
+app.post('/api',(req, res) =>{
 
 });
 
-app.delete('/',(req, res) =>{
+app.put('/api',(req, res) =>{
 
 });
+
+app.delete('/api',(req, res) =>{
+
+});
+
+app.listen(port, ()=> console.log(`listening to port ${port}`));
