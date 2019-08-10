a1c6adf8292f9e8df4697fe7525ada1c502065d5
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-08-10 11:59:48 -0700

    rename dev file to dist, create bundle.js

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+    //functions
+  }
+
+
+  componentDidMount(){}
+
+  render(){
+    return(
+      <div>test react element</div>
+    );
+  }
+}
+
+export default App;
+
+
+
