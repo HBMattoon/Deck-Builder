40447337b27c82ec6b7541fe91d1130b4d8eace5
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-08-10 18:02:25 -0700

    create script to initialize/reset databases

---
+++ database/reset.js
@@ -0,0 +1,24 @@
+const {Client} = require('pg');
+const client = new Client(
+  {
+    database: 'deckbuilder',
+  }
+)
+client.connect();
+
+client.query(`DROP TABLE IF EXISTS decks`)
+client.query(`DROP TABLE IF EXISTS searchHist`)
+
+client.query(`CREATE TABLE decks (
+  id serial NOT NULL,
+  owner_id character varying(50),
+  deck_list character varying(200),
+  CONSTRAINT urls_pkey PRIMARY KEY (id)
+)`);
+
+client.query(`CREATE TABLE searchHist (
+  id serial NOT NULL,
+  query jsonb,
+  response jsonb,
+  CONSTRAINT hist_pkey PRIMARY KEY (id)
+)`);
