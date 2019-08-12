4b1f824b25d7452e03161e94c15593c7c084170c
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-08-12 15:53:59 -0700

    add card display zone

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,59 @@
 import React from 'react';
+import SearchBar from './components/search_bar.jsx';
+import Display from './components/display.jsx';
 
 
 
 class App extends React.Component {
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      currentSearch: '',
+      searchResult: [],
+
+    };
     //functions
     this.getCard = this.getCard.bind(this);
+
   }
 
 
   componentDidMount(){
-    this.getCard({name:"black lotus"});
+    // this.getCard({name:"miner's bane"});
   }
 
   getCard(query){
-    let jsonQuery = JSON.stringify(query)
+
+
+    let jsonQuery = JSON.stringify(query);
+
+    jsonQuery = jsonQuery.toLowerCase();
+
     console.log(jsonQuery);
     fetch(`/api/mtg/?params=${jsonQuery}`)
     .then(cards => cards.json())
-    .then(cards => console.log(cards[0].text))
+    .then(cards => {
+      if(cards === []){
+        console.log('no results TODO')
+        query = [{name:'No Card Matches'}];
+      }
+      console.log('is array? ' + Array.isArray(cards));
+      this.setState({
+        'currentSearch':query,
+        'searchResult':cards
+      });
+      //console.log(cards[0].text)
+    })
     .catch(err => console.log('error!: ' + err))
   }
 
   render(){
     return(
-      <div>test react element</div>
+
+      <div>
+        <SearchBar getCard={this.getCard}/>
+        <Display cards={this.state.searchResult}/>
+      </div>
     );
   }
 }
