c96bc95eb7ed89d69e6734ed28805cc5033014b0
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-08-13 10:40:06 -0700

    change schema to use jsonb for deck storage

--- database/reset.js
+++ database/reset.js
@@ -7,18 +7,18 @@ const client = new Client(
 client.connect();
 
 client.query(`DROP TABLE IF EXISTS decks`)
-client.query(`DROP TABLE IF EXISTS searchHist`)
+//client.query(`DROP TABLE IF EXISTS searchHist`)
 
 client.query(`CREATE TABLE decks (
   id serial NOT NULL,
   owner_id character varying(50),
-  deck_list character varying(200),
+  deck_list jsonb,
   CONSTRAINT urls_pkey PRIMARY KEY (id)
 )`);
 
-client.query(`CREATE TABLE searchHist (
-  id serial NOT NULL,
-  query jsonb,
-  response jsonb,
-  CONSTRAINT hist_pkey PRIMARY KEY (id)
-)`);
+// client.query(`CREATE TABLE searchHist (
+//   id serial NOT NULL,
+//   query jsonb,
+//   response jsonb,
+//   CONSTRAINT hist_pkey PRIMARY KEY (id)
+// )`);
