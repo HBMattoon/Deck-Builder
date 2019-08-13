55df2cce52c9efd9a99eb376f759d23cbcdf08f3
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-08-13 10:38:11 -0700

    add savedeck function to fetch to server

--- client/src/components/display.jsx
+++ client/src/components/display.jsx
@@ -1,19 +1,53 @@
 import React from 'react';
 import CardDetails from './cardDetails.jsx';
 import CardDisplay from './cardDisplay.jsx';
+import Deck from './deck.jsx';
 
 let currentCard = {};
 class Display extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      cardSelection: [],
+      cardCollection: [],
       currentCard: {
       },
     }
     this.hoveredCard = this.hoveredCard.bind(this);
+    this.addToCollection = this.addToCollection.bind(this);
+    this.saveDeck = this.saveDeck.bind(this);
   }
 
+  //save new deck, or update old one
+  saveDeck(id){
+    if(id){
+      //update
+    } else {
+      //post
+      console.log('here we go')
+      let collection = this.state.cardCollection
+      console.log(collection)
+      // let query = {"deck": collection}
+      let jsonQuery = JSON.stringify(collection);
+      console.log(jsonQuery);
+      fetch(`/api/deck/`,{method: 'POST', body: JSON.stringify(collection)})
+      .then(res => res.json())
+      .then(res => {
+        console.log('look what i got! ' + JSON.stringify(res));
+      })
+      .catch(err => console.log( err));
+    }
+  }
+
+  loadDeck(id){
+    //get
+
+
+  }
+
+  deleteDeck(id){
+    //delete
+
+  }
 
 
   hoveredCard(card){
@@ -24,13 +58,24 @@ class Display extends React.Component{
     this.setState({'currentCard': card});
   }
 
+  addToCollection(card){
+    let arr = this.state.cardCollection;
+    console.log(arr);
+    if(card.name){
+      arr.push(card);
+      this.setState({cardCollection: arr})
+    }
+  }
+
   render(){
     return(
       <div id="mainDisplay">
         {console.log(this.props.cards[0])}
-
         <CardDisplay cards={this.props.cards} hoveredCard={this.hoveredCard}/>
-        <CardDetails card={this.state.currentCard}/>
+        <div className="sidebar">
+          <CardDetails add={this.addToCollection} card={this.state.currentCard}/>
+          <Deck saveDeck={this.saveDeck} currentCard={this.hoveredCard} collection={this.state.cardCollection}/>
+        </div>
       </div>
     )
   }
