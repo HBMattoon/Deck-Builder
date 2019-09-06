92d7b8bf5aa7cd93555d15b67ffaab8a0d4a4a72
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-09-06 16:52:44 -0700

    add rate limiter to provent API from being overloaded

--- server/controller/mtgFinder.js
+++ server/controller/mtgFinder.js
@@ -1,4 +1,5 @@
 const mtg = require('mtgsdk');
+const fetch = require('node-fetch');
 
 
 const findCard = (searchParams, callback) => {
@@ -12,8 +13,40 @@ const findCard = (searchParams, callback) => {
 }
 
 
+//card queue in order to prevent api overuse
+
+
+
+// let testArr = [];
+
+// const getNext = (cb) => {
+//   if(testArr.length > 0){
+//     setTimeout(() => {
+//       console.log(testArr.pop())
+//       getNext(cb);
+//     }, 1000);
+//   } else {
+//     console.log('theEnd')
+//     cb()
+//   }
+// }
+
+const newFindCard = (searchTerms, cb) => {
+
+  console.log('pong')
+
+  fetch(`https://api.scryfall.com/cards/search?q=c%3Awhite+cmc%3D1`)
+  .then(res => res.json())
+  .then(res => {
+    console.log(res);
+    cb('test');
+  })
+  .catch(err => {console.log('error!: ' + err)});
+}
+
 module.exports = {
   findCard,
+  newFindCard
 }
 
 
--- server/index.js
+++ server/index.js
@@ -1,16 +1,25 @@
 const express =  require('express');
 const path = require('path');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
+const limiter = require('express-rate-limit')
 // const { findCard } = require('./controller/mtgFinder.js')
-const { checkHist, createDeck } = require('./controller/database.js')
+const { checkHist, createDeck } = require('./controller/database.js');
+const { newFindCard } = require('./controller/mtgFinder.js');
 
+const rateLimit = require("express-rate-limit");
 
 const port = 3000;
 
 const app = express();
 
-app.use('/', express.static(path.join(__dirname, './../client/dist')));
+const apiLimiter = rateLimit({
+  windowMs: 100, // 15 minutes
+  max: 1
+});
 
+app.use('/', express.static(path.join(__dirname, './../client/dist')));
+app.use(bodyParser.json());
+app.use("/api/test", apiLimiter);
 
 app.get('/api/mtg',(req, res) =>{
   let queryParams = req.query.params;
@@ -30,9 +39,30 @@ app.get('/api/deck',(req, res) => {
 
 })
 
+
+
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+// app.set('trust proxy', 1);
+
+
+// only apply to requests that begin with /api/
+
+
+
+let test = 1;
+app.get('/api/test', (req, res) => {
+  test = test + 1;
+  newFindCard(test,(val) => {
+    res.send(val).end();
+  });
+})
+
 app.post('/api/deck',(req, res) =>{
   console.log('got post request');
-  let queryParams = req.query.params;
+  let queryParams = req.body;
+  console.log(queryParams)
+
   //queryParams = JSON.parse(queryParams);
   createDeck(queryParams,(id)=>{
     console.log('returning id: ' + id);
