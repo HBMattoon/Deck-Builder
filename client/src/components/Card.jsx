fe6c67cf68c390bfe18ecf1c6bd1f0751995825b
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-09-17 20:44:24 -0700

    update card to use react state

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -3,40 +3,65 @@
  let cardBG = 'https://gamepedia.cursecdn.com/mtgsalvation_gamepedia/0/07/Cardback_yellow.jpg?version=bb6894599b5370bc95db2af9c1f6dbf9';
  let key = 0;
  let distractionMode = false;
+ let face = 0;
  //if true, does not show cards in card zone, else card faces are shown
 
  class Card extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      card: {},
+      card: undefined,
+      //cardFace: undefined,
+      image: undefined,
       offset: -30
     }
   }
 
-  // componentDidMount(){
-  //   this.setState({offset: 0});
-  // }
+  componentDidMount(){
+    //this.getCardState();
+    //console.log('new mount')
+    this.setState({card: this.props.card});
+    this.updateCardImage(this.props.card, 0);
+  }
 
-  getCardImage(card){
+  componentWillReceiveProps(nextProps) {
+    if(this.props != nextProps) {
+      //console.log('updated mount')
+      this.setState({
+        card: nextProps.card
+      });
+
+      this.updateCardImage(nextProps.card, 0);
+    }
+  }
+
+
+  updateCardImage(card, face){
     //let card = this.props.card;
     let image_uri = '';
-    let name = '';
+    //let name = '';
     if(card.card_faces && !card.image_uris){
-      if(card.card_faces[0].image_uris ){
-        image_uri = card.card_faces[0].image_uris.large
+      if(card.card_faces[face].image_uris ){
+        image_uri = card.card_faces[face].image_uris.large
       } else {
-        console.log('the hell is this: ', card.card_faces[0])
+        console.log('what this: ', card.card_faces[face])
       }
-      name = card.card_faces[0].name
+      //name = card.card_faces[0].name
     } else {
       image_uri = card.image_uris.large
-      name = card.name
+      //name = card.name
     }
-    //this.setState({card: card})
-    //console.log('state is: ', this.state.card);
-    return image_uri;
+    //return image_uri;
+    //console.log('getting image!')
+    this.setState({image: image_uri});
   }
+
+  // getCardState(){
+  //   let currentCard = this.props.card;
+  //   if(currentCard.card_faces !== undefined){
+  //     this.setState({cardFace: currentCard.card_faces[0]})
+  //   }
+  // }
 
   slideButton(val){
     let buttonTransform = {
@@ -53,12 +78,40 @@
     this.setState({offset: -30})
   }
 
+  flipFace(){
+    if(face === 0){
+      face = 1;
+      this.updateCardImage(this.state.card, face);
+    } else {
+      face = 0;
+      this.updateCardImage(this.state.card, face);
+    }
+  }
+
+
+  // currentCard(){
+
+
+  //   console.log('test getting called!')
+  //   if(this.state.cardFace){
+  //     console.log('state exists, returning', this.state.cardFace)
+  //     return this.state.cardFace;
+  //   } else {
+  //     console.log('returning props', this.props.card)
+  //     return this.props.card;
+  //   }  //return this.state.card ? this.state.card : this.props.card
+  // }
+
 
   render(){
     return (
       <div key={this.props.keyval} className="test" onMouseEnter={() => this.showFlip()} onMouseLeave={() => this.hideFlip()}>
-        <div style={this.slideButton(this.state.offset)} className="button">test</div>
-        <img onClick={() => this.props.hoveredCard(this.props.card)} className="cards" src={distractionMode ? cardBG : this.getCardImage(this.props.card)} alt={this.props.card.name}></img>
+        <div style={this.slideButton(this.state.offset)} className="button">
+          <div className="drop_down_button" onClick={() => this.flipFace()}>
+            flip
+          </div>
+        </div>
+        <img onClick={() => this.props.hoveredCard(this.state.card)} className="cards" src={distractionMode ? cardBG : this.state.image}></img>
       </div>
     )
   }
