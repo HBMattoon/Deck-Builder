0cff992c1ad427df882cb3573c2a1921b5ed96f5
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-10-05 18:03:48 -0700

    update card detail options, begin process to get dual faced cards to display

--- client/src/components/cardDetails.jsx
+++ client/src/components/cardDetails.jsx
@@ -8,6 +8,7 @@ let options = [
   ['image', 'Card Image'],
   ['name','Name'],
   ['mana','Mana Cost'],
+  ['type', 'Spell Type'],
   ['text','Oracle Text'],
   ['pt','Power & Toughness'],
   ['flavor','Flavor Text'],
@@ -22,20 +23,33 @@ class CardDetails extends React.Component{
     super(props);
     this.state = {
       currentCard: {name: 'cool'},
+      face: 0,
       viewOptions: false,
+      image: true,
       name: true,
       mana: true,
       text: true,
       pt: true,
       flavor: true,
+      type: true,
       rarity: true,
       set: true,
-    }
+    };
+    this.myRef = React.createRef();
+  }
 
-    this.getMana = this.getMana.bind(this);
-    this.getPT = this.getPT.bind(this);
-    this.getText = this.getText.bind(this);
-    // this.parseText = this.parseText.bind(this);
+  //reset scroll position for details box on card change
+  componentDidMount(){
+    this.myRef.current.scrollTo(0,0);
+  }
+  componentDidUpdate(){
+    this.myRef.current.scrollTo(0,0);
+  }
+
+  addCard(){
+    if(this.props.card){
+      this.props.add(this.props.card);
+    }
   }
 
   //TODO fix icon rarity from being in same shap as last icon with same rarity
@@ -44,151 +58,74 @@ class CardDetails extends React.Component{
     return test
   }
 
-  // manaParse(manaStr = this.props.card.mana_cost){
-  //   // let manaStr = this.props.card.mana_cost
-  //   manaStr = manaStr.slice(1, manaStr.length-1); //remove curly braces at the ends
-  //   let manaArr = manaStr.split('}{')
-  //   manaArr = manaArr.map((val)=>{
-  //     val = val.toLowerCase().replace('/','')
-  //     if(val === 't'){
-  //       val = 'tap'
-  //     }
-  //     return val;
-  //   });
-  //   return(
-  //     manaArr.map((val)=> <i key={key++} className={`ms ms-${val} ms-cost ms-shadow`}></i>)
-  //   )
-  // }
-
   getMana(){
-    if(this.props.card.mana_cost){
-      return <div>Mana Cost: {parseCardText(this.props.card.mana_cost)}</div>;
+    let card = this.props.card;
+    //console.log(this.props.card.card_face)
+    if(card.card_faces && !card.image_uris){
+      card = card.card_faces[this.state.face];
+      console.log('twofaced card!')
+    }
+    if(card.mana_cost && this.state.mana){
+      return <div><span className="boldWord">Mana Cost: </span> {parseCardText(card.mana_cost)}</div>;
     }
   }
 
   getPT(){
-    if(this.props.card.power){
-      return <div className="marginTop10">P/T: {this.props.card.power}/{this.props.card.toughness}</div>;
+    if(this.props.card.power && this.state.pt){
+      return <div className="marginTop10"><span className="boldWord">P/T:</span> {this.props.card.power}/{this.props.card.toughness}</div>;
     }
   }
 
-  // //hello{1}{2}, my name is {r2} test
-  // parseCardText(text){
-  //   let arr = text.split('');
-  //   let result = [];
-  //   let current = [];
-  //   let i = 0;
-  //   while(arr.length > 0){
-  //     //console.log('does ↵ = ',JSON.stringify(arr[0]), '? ', arr[0] === '\n');
-  //     if(arr[0] === '{' ){
-  //       result.push(current.join(''));
-  //       current = [];
-  //       current.push(arr.shift())
-  //     } else if(arr[0] === '}') {
-  //       current.push(arr.shift())
-  //       result.push(current.join(''));
-  //       current = [];
-  //     } else if(arr[0] === '(') {
-  //       result.push(current.join(''));
-  //       current = [];
-  //       current.push(arr.shift())
-  //     } else if(arr[0] === ')') {
-  //       current.push(arr.shift())
-  //       result.push(current.join(''));
-  //       current = [];
-  //     } else if(arr[0] === '\n'){
-  //       //console.log('test!')
-  //       result.push(current.join(''));
-  //       current = [];
-  //       //current.push(arr.shift())
-  //       result.push(arr.shift());
-  //       current = [];
-  //     } else {
-  //       current.push(arr.shift())
-  //     }
-  //   }
-  //   result.push(current.join(''));
-
-  //   //console.log('result is: ',result);
-  //   return result.map(item => {
-
-  //     if(item[0] === '{'){
-  //       return this.manaParse(item);
-  //     } else if(item[0] === '\n'){
-  //       return <br/>
-  //     } else if(item[0] === '(' || item[item.length - 1] === ')'){
-  //       return <span className="oracleRules">{item}</span>
-  //     }else {
-  //       return <span>{item}</span>
-  //     }
-  //   })
-  //   //
-  // }
-
   getText(){
-    if(this.props.card.oracle_text){
+    if(this.props.card.oracle_text && this.state.text){
       let result = parseCardText(this.props.card.oracle_text);
-      // console.log(result);
       result.unshift(<br/>);
-
       return result;
     }
   }
 
   getFlavor(){
-    if(this.props.card.flavor_text){
+    if(this.props.card.flavor_text && this.state.flavor){
       return(
         <div>
           <br />
-          <span>Flavor Text: </span>
+          <span className="boldWord">Flavor Text: </span>
           <span className="oracleRules">
             {this.props.card.flavor_text}
           </span>
         </div>
       )
-
-
-
     }
   }
-  getName(){
-    //console.log(options)
-    if(this.state.name){
-      if(this.props.card.name){
-        console.log('returning cardName')
-        return <div className="cardName">Name: {this.props.card.name} </div>;
-      } else {
-        return <h1>choose a card!</h1>
-      }
-    }
 
+  getName(){
+    if(this.props.card.name && this.state.name){
+      console.log(this.props.card)
+      return <div className="cardName"><span className="boldWord">Name:</span> {this.props.card.name} </div>;
+    }
   }
 
   getType(){
-    if(this.props.card.type_line){
-      return <div>Type: {this.props.card.type_line}</div>;
-    }
-  }
-  getRarity(){
-    if(this.props.card.rarity){
-      return <div><br/>Rarity: {this.props.card.rarity}</div>;
-    }
-  }
-  getSet(){
-    if(this.props.card.set){
-      return <div>Set: <i className={this.setIcon()}></i> [{this.props.card.set}] {this.props.card.setName} </div>;
+    if(this.props.card.type_line && this.state.type){
+      return <div><span className="boldWord">Type:</span> {this.props.card.type_line}</div>;
     }
   }
 
-  //todo what does this do?
-  update(){
-    if(this.props.card){
-      this.props.add(this.props.card);
+  getRarity(){
+    if(this.props.card.rarity && this.state.rarity){
+      return <div><br/><span className="boldWord">Rarity:</span> {this.props.card.rarity}</div>;
     }
   }
+
+  getSet(){
+    if(this.props.card.set && this.state.set){
+      return <div><span className="boldWord">Set:</span> <i className={this.setIcon()}></i> [{this.props.card.set}] {this.props.card.setName} </div>;
+    }
+  }
+
 
   getImage(){
-    if(this.props.card.image_uris){
+    if(this.props.card.image_uris && this.state.image){
       return(
         <img className="resize" src={this.props.card.image_uris.large} alt={this.props.card.name}></img>
       )
@@ -201,7 +138,7 @@ class CardDetails extends React.Component{
         <div className="cardAndButtons">
           {this.getImage()}
         </div>
-        <div className="info">
+        <div className="paddingTop5">
           {this.getName()}
           {this.getMana()}
           {this.getType()}
@@ -217,18 +154,39 @@ class CardDetails extends React.Component{
 
   showOptions(){
     let key = 1;
-    return options.map((option) => {
-      let statePayload = {};
-      statePayload[option[0]] = !this.state[option[0]];
-      console.log
-      return(
-        <div key={key++} className="options">
-          <input type='checkbox' onChange={()=> {console.log(statePayload);
-            this.setState(statePayload)}} checked={this.state[option[0]]}></input>
-           <span>{option[1]}</span>
-           <br/>
-        </div>
-      )
+    return(
+      <div>
+        <button onClick={() => this.resetOption()}>Reset</button>
+        {
+          options.map((option) => {
+            let statePayload = {};
+            statePayload[option[0]] = !this.state[option[0]];
+            console.log
+            return(
+              <div key={key++} className="options">
+                <input type='checkbox' onChange={()=> {console.log(statePayload);
+                  this.setState(statePayload)}} checked={this.state[option[0]]}></input>
+                 <span>{option[1]}</span>
+                 <br/>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+
+  resetOption(){
+    this.setState({
+      image: true,
+      name: true,
+      mana: true,
+      text: true,
+      pt: true,
+      flavor: true,
+      type: true,
+      rarity: true,
+      set: true,
     })
   }
 
@@ -237,12 +195,13 @@ class CardDetails extends React.Component{
 
       <div className="cardDetailsZone">
         <div className="cardDetailOptions">
-            <button onClick={()=>{this.update()}}>Add</button>
+            <button onClick={()=>{this.addCard()}}>Add</button>
             <button>Remove</button>
             <button onClick={()=> this.setState({viewOptions: !this.state.viewOptions})}>Display Options</button>
         </div>
-        <div className="cardDetails">
+        <div ref={this.myRef} className="cardDetails">
           {this.state.viewOptions ? this.showOptions() : this.showInfo()}
+          <br/>
         </div>
       </div>
     )
